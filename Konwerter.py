136422167c1260bd7f92634aad0376a0db9b764d
Remigiusz48749 <dsw48749@student.dsw.edu.pl>
2023-05-24 13:06:00 +0200

    Task1 dodano: wybór typu konwersji i łatane ewentułalne błędy

---
+++ Konwerter.py
@@ -0,0 +1,51 @@
+import sys
+
+
+def xml_to_json(xml_path, json_path):
+    pass
+
+
+def json_to_xml(json_path, xml_path):
+    pass
+
+
+def json_to_yaml(json_path, yaml_path):
+    pass
+
+
+def yaml_to_json(yaml_path, json_path):
+    pass
+
+
+def main():
+    if len(sys.argv) != 3:
+        print("Sposób użycia: program.exe pathFile1.x pathFile2.y")
+        print("gdzie x i y to jeden z formatów .xml, .json i .yml (.yaml)")
+        return
+
+    in_path = sys.argv[1]
+    out_path = sys.argv[2]
+
+    if in_path.endswith(".xml") and out_path.endswith(".json"):
+        xml_to_json(in_path, out_path)
+        print("Konwersja zakończona pomyślnie.")
+    elif in_path.endswith(".json") and out_path.endswith(".xml"):
+        json_to_xml(in_path, out_path)
+        print("Konwersja zakończona pomyślnie.")
+    elif in_path.endswith(".json") and out_path.endswith(".yml"):
+        json_to_yaml(in_path, out_path)
+        print("Konwersja zakończona pomyślnie.")
+    elif in_path.endswith(".yml") and out_path.endswith(".json"):
+        yaml_to_json(in_path, out_path)
+        print("Konwersja zakończona pomyślnie.")
+    else:
+        print("""Nieobsługiwany format pliku wejściowego i/lub wyjściowego.
+        Obsługiwane formaty to: .xml, .json i .yml (.yaml).
+        Upewnij się, że podałeś poprawne rozszerzenia plików i spróbuj ponownie.
+        Przykładowe wywołanie programu:
+        Konwerter.exe plik_wejsciowy.xml plik_wyjsciowy.json
+        """)
+
+
+if __name__ == "__main__":
+    main()
